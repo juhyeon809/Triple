9de73d9ac7b7b49bf4d76fca2e233aa03817fcb9
BBIIN <tiktok0416@hanmail.net>
2022-07-03 18:02:11 +0900

    관리자페이지 메뉴

---
+++ admin/script.js
@@ -0,0 +1,28 @@
+const allDropdown = document.querySelectorAll('#sidebar .side-dropdown');
+
+allDropdown.forEach(item => {
+    const a = item.parentElement.querySelector('a:first-child');
+    a.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		if(!this.classList.contains('active')) {
+			allDropdown.forEach(i=> {
+				const aLink = i.parentElement.querySelector('a:first-child');
+
+				aLink.classList.remove('active');
+				i.classList.remove('show');
+			})
+		}
+
+		this.classList.toggle('active');
+		item.classList.toggle('show');
+	})
+})
+
+const allProgress = document.querySelectorAll('main .card .progress');
+allProgress.forEach(item=> {
+	item.style.setProperty('--value', item.dataset.value)
+})
+
+
+
