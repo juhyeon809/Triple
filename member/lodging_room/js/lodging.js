99223e168fc8f1bb119ef42c93ee5943943c0be7
Sim J <polaris_ui@hanmail.net>
2022-07-18 21:24:44 +0900

    css, js 수정

--- member/lodging_room/js/lodging.js
+++ member/lodging_room/js/lodging.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function sendit(){
     const name = document.getElementById('name');
     const hp = document.getElementById('hp');
--- member/lodging_room/js/payment.js
+++ member/lodging_room/js/payment.js
@@ -1,3 +1,5 @@
+'use strict'
+
 $("#check_module").click(function () {        
     var IMP = window.IMP; // 생략가능        
     IMP.init('import');
