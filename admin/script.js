dbf4c58b83d055f31a6d3028f4feba45bd94fcb1
BIIN <tiktok0416@hanmail.net>
2022-07-21 17:06:09 +0900

    수정

--- admin/script.js
+++ admin/script.js
@@ -26,21 +26,21 @@ allProgress.forEach(item=> {
 })
 
 
-/*상품등록*/
-function divplus(){
-	const parent = document.getElementById('plusbox')
-	const originItem = document.getElementById('plusbox')
-	parent.appendChild(originItem.cloneNode(true));
-} 
+// /*상품등록*/
+// function divplus(){
+// 	const parent = document.getElementById('plusbox')
+// 	const originItem = document.getElementById('plusbox')
+// 	parent.appendChild(originItem.cloneNode(true));
+// } 
 
-function showPopup(){
-	let popupX = (window.screen.width/2)-(400/2);
-	let popupY = (window.screen.height/2) - (300/2);
-	window.open("roomPop.html","팝업 테스트","width=400, height=300, top="+popupY+ ", left="+popupX+",screenX="+popupX+",screenY="+popupY);
-}
+// function showPopup(){
+// 	let popupX = (window.screen.width/2)-(400/2);
+// 	let popupY = (window.screen.height/2) - (300/2);
+// 	window.open("roomPop.html","팝업 테스트","width=400, height=300, top="+popupY+ ", left="+popupX+",screenX="+popupX+",screenY="+popupY);
+// }
 
-function showPopup1(){
-	let popupX = (window.screen.width/2)-(400/2);
-	let popupY = (window.screen.height/2) - (300/2);
-	window.open("pop-internet.html","팝업 테스트","width=400, height=300, top="+popupY+ ", left="+popupX+",screenX="+popupX+",screenY="+popupY);
-}
+// function showPopup1(){
+// 	let popupX = (window.screen.width/2)-(400/2);
+// 	let popupY = (window.screen.height/2) - (300/2);
+// 	window.open("pop-internet.html","팝업 테스트","width=400, height=300, top="+popupY+ ", left="+popupX+",screenX="+popupX+",screenY="+popupY);
+// }
